b137df634ff70675ca93e0cf0efa0baf7e22a164
Sohan Dutta <sohan@codebuddy.co>
2023-10-01 20:17:03 +0530

    :sparkles: feat(ui): checkbox storyboard

--- src/stories/Checkbox/Checkbox.stories.svelte
+++ src/stories/Checkbox/Checkbox.stories.svelte
@@ -6,7 +6,21 @@
 <Meta title="Components/Checkbox" component={Checkbox} />
 
 <Template let:args>
-	<Checkbox {...args}>Label</Checkbox>
+	<Checkbox {...args}>Normal</Checkbox>
+	<Checkbox {...args} description="Lorem Isum de color">With Description</Checkbox>
+	<Checkbox {...args} description="Lorem Isum de color" error="required">With Error</Checkbox>
+	<Checkbox {...args} description="Lorem Isum de color" error="required" labelPosition="left">
+		Left
+	</Checkbox>
+	<Checkbox
+		{...args}
+		description="Lorem Isum de color"
+		error="required"
+		labelPosition="left"
+		disabled
+	>
+		Disabled
+	</Checkbox>
 </Template>
 
 <Story name="Variants" />
